4ae066ce0ce07c8e3806959c285ff99a55223c9d
TJ <THammons@gmail.com>
2023-11-07 21:52:54 -0600

    clean coments out of lightsout

--- two-move/src/board-builders/lights-out.ts
+++ two-move/src/board-builders/lights-out.ts
@@ -1,4 +1,4 @@
-import { CellType, IBoard, IBoardBuilderOption, ICell, ICellUpdateEvent, IMapItem, ItemLocation } from "../types.js";
+import { CellType, IBoard, IBoardBuilderOption, IMapItem, ItemLocation } from "../types.js";
 import { BoardValidation } from "../board/validation.js";
 
 export class LightsOut<T extends IBoard> implements IBoardBuilderOption<T> {
@@ -55,24 +55,16 @@ export class LightsOut<T extends IBoard> implements IBoardBuilderOption<T> {
             return board;
     }
 
-    getUpdatedBoard(board: T, targetLocation: ItemLocation, override?: number, markSeen: boolean = true): T {
+    private getUpdatedBoard(board: T, targetLocation: ItemLocation, override?: number, markSeen: boolean = true): T {
         const cells = board.getCells();
-        // for(var i = 0; i < cells.length; i++) {
-        //     cells[i].classes = cells[i].classes.filter(c => c != 'fog');
-        // }
+
         const distanceToPlayer = override !== undefined ? override : (this.lightRadius);
         const visibleLocations = BoardValidation.locationsVisibleToPlayer(targetLocation, distanceToPlayer, cells, board.width, board.height);
-
-        let updatedCells: ItemLocation[] = []
-        // console.log('prefog', cells);
 
         //TODO optionally narrow down cells based on target location
         cells.forEach((cell, index, array) => {
             let updated = false;
-            // cell.indicator == 'X' ? cell.indicator = ' ' : cell.indicator;
-            // cell.classes = cell.classes.filter(c => c != 'fog');
             const isVisibleToPlayer = visibleLocations.includes(index);
-            //console.log('isvisibletoplayer', isVisibleToPlayer, index, visibleLocations, cell);
             const hasKeepIluminatedItem = cell.mapItems.map(m => m.cellType).some(c => this.iluminate.includes(c));
 
             if (isVisibleToPlayer && markSeen) {
@@ -80,7 +72,6 @@ export class LightsOut<T extends IBoard> implements IBoardBuilderOption<T> {
                     cell.classes.push('seen');
                     cell.classes = cell.classes.filter(c => c != 'unseen');
                     updated = true;
-                    //console.log('seen')
                 }
             }
 
@@ -88,38 +79,20 @@ export class LightsOut<T extends IBoard> implements IBoardBuilderOption<T> {
                 if (!cell.classes.includes('fog')) {
                     cell.classes.push('fog');
                     updated = true;
-                    //console.log('fogging', index, distanceToPlayer > 0, !isVisibleToPlayer, !hasKeepIluminatedItem)
-                    // cell.indicator = 'X';
                 }
             }
             else {
                 if (cell.classes.includes('fog')) {
-                    //cell.classes.splice(cell.classes.indexOf('fog'), 1);
                     array[index].classes = cell.classes.filter(c => c != 'fog');
                     updated = true;
-                    // cell.indicator = ' ';
                 }
             }
             if (updated) {
-                // console.log('setcell', JSON.stringify(cell));
-                // board.setCell(index, JSON.parse(JSON.stringify(cell))); 
                 board.updateCell(index);
-                //console.log('updatedcell', index, JSON.stringify(board.getCell(index)))
             }
         });
 
-        // for(var l of visibleLocations) {
-        //     console.log('visible', l, cells[l]);
-        //     cells[l].classes = cells[l].classes.filter(c => c != 'fog');
-        // }
-
-        // console.log('lightsout', JSON.stringify(cells))
-
-        // console.log('LightsOut > fogged', distanceToPlayer, visibleLocations, cells.filter(c => c.classes.includes('fog')).map((x, i) => i));
-        // board.setCells(cells);
-
         this.isInitialLoad = false;
         return board;
-
     }
 }
