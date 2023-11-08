ee617270b3972597a0c15358979003e27b3a012a
TJ <THammons@gmail.com>
2023-11-08 07:50:17 -0600

    setting up for deploy, add 404

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,34 @@ import { defineConfig, BuildOptions } from 'vite';
 const { resolve } = require('path');
 
 export default defineConfig({
+  plugins: [
+    {
+      name: 'replace-src-path',
+      apply: 'build',
+      enforce: 'post',
+      generateBundle(_, bundle) {
+        for (const outputItem of Object.values(bundle)) {
+          if (outputItem.fileName.endsWith('.html')) {
+            outputItem.fileName = outputItem.fileName.replace('two-move/html/', '')
+          }
+        }
+      }
+    }
+  ],
   build: {
+    assetsDir: 'dist',
+    outDir: 'dist',
     rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]',
+      },
       input: {
         main: resolve(__dirname, 'index.html'),
         home: resolve(__dirname, 'two-move/html/home.html'),
         game: resolve(__dirname, 'two-move/html/game.html'),
+        fourOhFour: resolve(__dirname, 'two-move/html/404.html'),
         blockly: resolve(__dirname, 'two-move/html/blockly-builder.html'),
         
       },
