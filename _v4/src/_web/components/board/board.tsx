efa7e6be6722cd98978634f291b8a595153a866c
TJ <THammons@gmail.com>
2023-11-17 08:44:24 -0600

    unstyled cell no longer flashes on rerender

--- _v4/src/_web/components/board/board.tsx
+++ _v4/src/_web/components/board/board.tsx
@@ -1,10 +1,11 @@
-import { IMap } from "@/maps/types";
+import { IMap, IMapItem } from "@/maps/types";
 import './cell';
-import { IMapItemCollection } from "./cell";
 
 //TODO this could be a lot more functional...
 export class BoardComponent extends HTMLElement {
-    cells: IMapItemCollection[] = [];
+    cells: Map<number, IMapItem[]> = new Map();
+    height: number = 10;
+    width: number = 10;
 
     connectedCallback() {
         this.attachShadow({ mode: 'open' });
@@ -27,49 +28,53 @@ export class BoardComponent extends HTMLElement {
     }
 
     populateCells(map?: IMap) {
+
+        //TODO: partial updates
+        if (!map) return;
+
         //TODO: This needs to be replaced.. probably wire to a board event?
-        this.cells = [];
         [...Array(100).keys()].forEach((i) => {
-            this.cells.push({
-                items: [{ type: 'empty', location: i }],
+            if (!this.cells)
+                this.cells = new Map();
+
+            this.cells.set(i, [{
+                type: 'empty',
+                location: i,
                 attributes: ['unseen']
-            });
+            }]);
         });
         // this.cells[0].items.push({ type: 'player', location: 0 });
         // this.cells[99].items.push({ type: 'goal', location: 99 });
-        
-        if (!map) return;
-        console.log('populateCells', map);
+
 
         for (let [key, value] of map.mapItems) {
             for (let item of value) {
-                this.cells[item.location].items = this.cells[item.location].items.filter((i) => i.type !== 'empty');
-                this.cells[item.location].items.push({ type: key, location: item.location, direction: item.direction });
-                this.cells[item.location].attributes = [...new Set([...(item.attributes || [])])];
+                const existingAttributes = this.cells.get(item.location)?.flatMap(c => c.attributes) || [];
+                const existingItems = this.cells.get(item.location)?.filter(c => c.type !== 'empty') || [];
+                this.cells.set(item.location, [...existingItems, {
+                    id: item.id,
+                    type: key,
+                    location: item.location,
+                    direction: item.direction,
+                    attributes: [...new Set([...existingAttributes, ...(item.attributes || [])])]
+                } as IMapItem
+                ]);
             }
         }
-        
     }
 
 
     render() {
+        let cellDisplayItems = [];
+        for (let [key, value] of this.cells) {
+            cellDisplayItems.push({ location: key, items: value });
+        }
+
         //TODO: set style based on board options (cell size, min-width, gridOptions etc)
         const renderThis = `
-        <style>
-        .board {
-            display: grid;
-            grid-template-columns: repeat(auto-fill, minmax(50px, 1fr));
-            grid-auto-rows: 50px;
-            gap: 0px;
-
-            width: 520px;
-            min-width: 520px;
-            font-size: 45px;
-
-        }
-        </style>
+        <link rel="stylesheet" href="/src/_web/components/board/cell.css">
         <div class="board">
-        ${this.cells.map((cell, index) => `
+        ${cellDisplayItems.map((cell, index) => `
         <two-move-cell id="cell-comp-${index}"></two-move-cell>
         `).join('')}
         </div>
--- _v4/src/_web/components/board/cell.tsx
+++ _v4/src/_web/components/board/cell.tsx
@@ -1,9 +1,5 @@
 import { AttributeType, IMapItem } from "@/maps/types";
 
-export interface IMapItemCollection {
-    items: IMapItem[];
-    attributes: AttributeType[];
-}
 
 const directionIndicators = new Map([
     ['north', '^'],
@@ -13,25 +9,23 @@ const directionIndicators = new Map([
 ]);
 
 export class CellComponent extends HTMLElement {
-    private _cell: IMapItemCollection = {
-        items: [],
-        attributes: []
-    };
+    private _cell: IMapItem[] = [];
     private _needsRender = false;
 
     constructor() {
         super();
-        this.attachShadow({ mode: 'open' });
+        // this.attachShadow({ mode: 'open' });
     }
 
-    set cellData(data: IMapItemCollection) {
+    set cellData(data: IMapItem[]) {
         this._cell = data;
-        if (this.shadowRoot) {
-            this.render();
-        }
-        else {
-            this._needsRender = true;
-        }
+        // if (this.shadowRoot) {
+        //     this.render();
+        // }
+        // else {
+        //     this._needsRender = true;
+        // }
+        this.render();
         // console.log('set cell data', data, this._needsRender)
     }
 
@@ -45,37 +39,35 @@ export class CellComponent extends HTMLElement {
     buildCell(): string {
         const cellElement = document.createElement('div');
         cellElement.classList.add('board-cell');
-        cellElement.classList.add('cell-' + this._cell.items[0].location);
+        cellElement.classList.add('cell-' + this._cell[0].location);
         cellElement.classList.add('cell');
         let playerItem: IMapItem | undefined = undefined;
-        this._cell.items.forEach((item) => {
+        this._cell.forEach((item) => {
             cellElement.classList.add(item.type);
             if (item.type === 'player') {
                 playerItem = item;
             }
+
+            item.attributes?.forEach((attribute: string) => {
+                cellElement.classList.add(attribute);
+            });
         });
         if (playerItem !== undefined) {
-            console.log(playerItem)
             let indicator = '';
             const player = playerItem as IMapItem;
-                indicator = directionIndicators.get(player.direction ?? 'north') ?? '';
+            indicator = directionIndicators.get(player.direction ?? 'north') ?? '';
             cellElement.innerHTML = indicator;
         }
 
-        this._cell.attributes.forEach((attribute: string) => {
-            cellElement.classList.add(attribute);
-        });
-        
         // console.log(cellElement.outerHTML)
         return cellElement.outerHTML;
     }
 
     render() {
-        const renderThis = `
-        <link rel="stylesheet" href="/src/_web/components/board/cell.css">
-        ${this.buildCell()}`;
+        const renderThis = `${this.buildCell()}`;
         // console.log(renderThis)
-        this.shadowRoot!.innerHTML = renderThis;
+        // this.shadowRoot!.innerHTML = renderThis;
+        this.innerHTML = renderThis;
     }
 
 }
