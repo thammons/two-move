e78e5d1975605aef88593202dc90c1aea3711fba
TJ <THammons@gmail.com>
2023-11-21 07:39:43 -0600

    user mover prettier and import cleanup

--- _v4/src/movers/user-mover.ts
+++ _v4/src/movers/user-mover.ts
@@ -1,17 +1,15 @@
-import { IMap } from "@/maps";
-import * as MapFunctions from "@/maps";
+import { IMap, checkPlayerCollision, movePlayer, turnPlayer } from "@/maps";
 import { MoveTypes } from "./types";
 
-
 export class UserMover {
-
     getNextMove(move: MoveTypes, map: IMap): IMap {
         let newMap: IMap = map;
+
         switch (move) {
-            case 'move-forward':
+            case "move-forward":
                 newMap = this.move(map);
                 break;
-            case 'turn':
+            case "turn":
                 newMap = this.turn(map);
                 break;
         }
@@ -20,18 +18,15 @@ export class UserMover {
     }
 
     move(map: IMap): IMap {
-        let c = MapFunctions.checkPlayerCollision(map);
+        let c = checkPlayerCollision(map);
         let newMap: IMap = c.map;
         if (!c.isCollision) {
-            newMap = MapFunctions.movePlayer(newMap);
+            newMap = movePlayer(newMap);
         }
         return newMap;
     }
 
     turn(map: IMap): IMap {
-        return MapFunctions.turnPlayer(map);
+        return turnPlayer(map);
     }
-
 }
-
-
